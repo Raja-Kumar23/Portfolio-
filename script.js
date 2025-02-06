b1fe333ed88e2e6cc9cdb645bf2fbb89fc33e702
Raja Kumar <sahrajakumar885@gmail.com>
2025-02-06 15:59:35 +0530

    Hero Page Completed

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+
 document.addEventListener("DOMContentLoaded", function () {
     const menuIcon = document.querySelector(".menu-icon");
     const navLinks = document.querySelector(".nav-links");
 
-    menuIcon.addEventListener("click", function () {
+    menuIcon.addEventListener("click", function (e) {
+        e.stopPropagation();
         navLinks.classList.toggle("active");
+    });
+
+    document.addEventListener("click", function (e) {
+        if (!navLinks.contains(e.target) && !menuIcon.contains(e.target)) {
+            navLinks.classList.remove("active");
+        }
     });
 });
